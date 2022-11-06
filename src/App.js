993f72a57b415c7b87692322ed0ec7ed990a3e9f
Souvik Mitra <mitrasouvik123@gmail.com>
2022-11-06 09:44:33 +0530

    added Transaction and TransactionList components and modified the rest

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import React from 'react'
 import Header from './Components/Header'
 import Balance from './Components/Balance'
 import IncomeExpense from './Components/IncomeExpense'
+import TransactionList from './Components/TransactionList'
+import AddTransaction from './Components/AddTransaction'
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
       <div className="container">
         <Balance />
         <IncomeExpense />
+        <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   )
